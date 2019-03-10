fa5618f8738f1326a6a57b73c2fa3527a02dbcb9
yoru722 <kichiru3161026@gmail.com>
2019-03-10 10:20:52 +0900

    分岐、ループの書き方追加

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
 print('hello world')
+
+str = 'a'
+
+print("数字を入力してください")
+# ユーザーに入力させるのは文字列で返却されるためキャスト整数型に変更する必要がある
+inte = int(input())
+
+# pythonでインデントが4つらしい
+if inte == 2:
+    print('2です')
+elif inte == 3:
+    print("３desu")
+else:
+    print('その他です')
+
+# pythonでは真偽値はTrue, Falseの様に先頭は大文字で記述すること
+integ = 0
+while True:
+    print(integ)
+    integ = integ + 1
+    if integ == 5:
+        break
+print("for文")
+# for文も使用できる
+for i in range(3, 5):
+    print(i)
+
