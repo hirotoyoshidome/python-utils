a25084d1cfa78b5f77ebfcde28a987773b64e36e
yoru722 <kichiru3161026@gmail.com>
2019-03-30 23:09:47 +0900

    多次元配列の計算のサンプルを追加

---
+++ neural_network/sample_tensor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+# 多次元配列の基本を実装する
+
+# 一次元配列
+A = np.array([1, 2, 3, 4])
+print(A)
+
+# 次元を出力する
+print(np.ndim(A))
+
+# 構造を出力する
+# タプルで出力する
+print(A.shape)
+# タプルのためインデックスで参照することができる
+print(A.shape[0])
+
+# 二次元配列
+B = np.array([[1, 2], [3, 4], [5, 6]])
+print(B)
+print(np.ndim(B))
+print(B.shape)
+
+
+# 行列の積
+# 要素数が一致していないと例外が発生する
+# ドット積を算出する
+C = np.array([[1, 2], [3, 4], [5, 6]])
+D = np.array([7, 8])
+# ドット積を算出
+# 内部的には1*7+2*8, 3*7+4*8, 5*7+6*8を計算している
+print(np.dot(C, D))
+
